6779ec5e4cc238721e290c700197d5c98621a169
Ce Ma <cma@barnard3.esat.kuleuven.be>
2025-02-01 21:39:53 +0100

    remove cycle cnt reg, mdf fill macro logic, remove corner case handling in unit

--- rtl/qkv_macro_handler_unit/src/qkv_macro_handler_unit_N_Mux_8_2_56_4.v
+++ rtl/qkv_macro_handler_unit/src/qkv_macro_handler_unit_N_Mux_8_2_56_4.v
@@ -32,3 +32,5 @@ module qkv_macro_handler_unit_N_Mux_8_2_56_4( in3, in2, ctrl1, out1 );
 endmodule
 
 
+
+
--- rtl/qkv_macro_handler_unit/src/qkv_macro_handler_unit_N_Muxb_1_2_98_4.v
+++ rtl/qkv_macro_handler_unit/src/qkv_macro_handler_unit_N_Muxb_1_2_98_4.v
@@ -31,3 +31,5 @@ module qkv_macro_handler_unit_N_Muxb_1_2_98_4( in2, ctrl1, out1 );
 endmodule
 
 
+
+
